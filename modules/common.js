20d9c23e30ed8c65cf6f13e8a8b4df0f6a62fa0a
Terence Yim <terencey@google.com>
2024-03-14 07:26:21 +0000

    Added BQML pipeline library for remote inference

---
+++ modules/common.js
@@ -0,0 +1,22 @@
+module.exports = {
+    /**
+     * Declares the given source as a resolvable Dataform data source. 
+     */
+    declare_resolvable: (source) => {
+        if (source.constructor === Object) {
+            declare(source);
+        } else {
+            declare({
+                name: source
+            });
+        }
+    },
+
+    /**
+     * Forms a SQL filter clause for filtering out retryable 
+     * error based on a given status column.
+     */
+    retryable_error_filter: (status_col) => {
+        return `${status_col} NOT LIKE 'A retryable error occurred:%'`;
+    },
+};
