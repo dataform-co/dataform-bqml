83b1880c5ae3078d20bcc21e7381b8b1d74341ed
Terence Yim <terencey@google.com>
2024-03-14 07:26:21 +0000

    Added BQML pipeline library for remote inference

---
+++ index.js
@@ -0,0 +1,5 @@
+const structured_table_ml = require("./modules/structured_table_ml");
+const object_table_ml = require("./modules/object_table_ml");
+
+module.exports = {...structured_table_ml, ...object_table_ml};
+
