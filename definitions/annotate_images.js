48b0db3acb4219098a160aa2dfd694d43a58701c
Terence Yim <terencey@google.com>
2024-03-29 21:43:20 +0000

    Remove unused files

--- definitions/annotate_images.js
+++
@@ -1,7 +0,0 @@
-const bqml = require("../index");
-
-let model = "vision";
-let obj_table = "imagesets_500k";
-let target_table = "df_annotated_imagesets_500k";
-
-bqml.annotate_image(obj_table, target_table, model, ['LABEL_DETECTION']);
